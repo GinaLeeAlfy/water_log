91d21029d8b3c5a8ccdcaa17043480b901e76f5c
Gina Alfandary Magana <ginaleealfy@gmail.com>
2024-02-13 09:14:51 -0800

    setting up browser routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Auth0Provider
-      domain={import.meta.env.VITE_AUTH0_DOMAIN}
-      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
-      authorizationParams={{ redirect_uri: "http://localhost:5173/callback" }}
-    >
-      <App />
-    </Auth0Provider>
+    <BrowserRouter>
+      <Auth0Provider
+        domain={import.meta.env.VITE_AUTH0_DOMAIN}
+        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+        authorizationParams={{ redirect_uri: "http://localhost:5173/callback" }}
+      >
+        <Routes>
+          <Route path="/" element={<App />} />
+        </Routes>
+      </Auth0Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
